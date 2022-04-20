b21aa6b176ff8ea0e35ebe27e886f9026d0ef864
Alejandro <alejandrom.am33@gmail.com>
2022-04-20 17:51:17 -0500

    add: index.js file

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const alex = new MissionCommander("Alex")
+const juan = new MissionCommander("Juan")
+console.log(carlo.name)
+console.log(alex.name)
+console.log(juan.name)
